03088f64e368c79d6fa1a3c02d1bc4085cb2aeea
polinadotio <psoshnin@gmail.com>
2016-06-04 18:20:17 -0700

    add graphql

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ Matchjs-mobile
 
 Mobile web client for MatchJS.
 
-Stack
-===========
+### Stack
 
 - Front-end
   - react
@@ -32,16 +31,16 @@ Stack
  - MongoDB with Mongoose
  - Immutable.js
 
-Starting
-===========
+### Starting
 
 - `npm start`
 
-Resources
-===========
+### Resources
 
 - http://slides.com/tejasmanohar/koa-es6#/
 - Isomorphic React + Relay rendering
+- Relay is a framework for building data-driven React applications.
+- You can declare your data requirements using GraphQL for each component and Relay handles the requests efficiently. Relay makes a few assumptions about the GraphQL schema that is provided by the GraphQL server.
 
 Authors
 =======
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import Router from 'koa-router';
 import qs from 'koa-qs';
 import parsebody from 'co-body';
 import cors from 'koa-cors';
-import logger from 'koa-logger'
+import logger from 'koa-logger';
+import {graphql} from 'graphql';
 
 let port = process.env.PORT || 3000;
 let routes = new Router();
@@ -12,13 +13,20 @@ const app = new Koa();
 //support nested query params
 qs(app);
 
-app.use(logger());
-app.use(cors());
-app.use(routes.middleware());
+app
+  .use(logger())
+  .use(cors())
+  .use(routes.routes())
+  .use(routes.allowedMethods())
+  .use(routes.middleware());
 
-app.use(function *() {
-  this.body = "Hello world heyy girlll";
-});
+routes
+  .get('/', function *(next) {
+    this.body = 'Hello World!';
+  })
+  .get('/graphql', function *(next) {
+    this.body = 'graphql!';
+  })
 
 app.listen(port, () => {
   console.log('app is listening on', port);
