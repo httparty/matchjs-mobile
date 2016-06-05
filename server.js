72e25c1e8edafe8178c93b183d3c399be117bd33
polinadotio <psoshnin@gmail.com>
2016-06-04 17:49:27 -0700

    add koa middlewares

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-import koa from 'koa';
-var app = koa();
+import Koa from 'koa';
+import Router from 'koa-router';
+import qs from 'koa-qs';
+import parsebody from 'co-body';
+import cors from 'koa-cors';
+import logger from 'koa-logger'
 
 let port = process.env.PORT || 3000;
+let routes = new Router();
+const app = new Koa();
+
+//support nested query params
+qs(app);
+
+app.use(logger());
+app.use(cors());
+app.use(routes.middleware());
 
 app.use(function *() {
   this.body = "Hello world heyy girlll";
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log('app is listening on', port);
+});
+
+module.exports = app;
